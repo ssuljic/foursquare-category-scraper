90a778ae464fbb7a6f67190f08be3bc0d111cf0c
ssuljic <sadzid.etf@gmail.com>
2016-01-14 23:55:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res) {
+  var url = 'https://developer.foursquare.com/categorytree';
+
+  request(url, function(error, response, html) {
+    if(!error) {
+      var $ = cheerio.load(html);
+      var map = {};
+
+      $('.categoryDescription').each(function (i, el) {
+        var name = $(this).find('.name').first().text().trim();
+        var id = $(this).find('.id').first().text().trim();
+        map[id] = name;
+      });
+
+      fs.writeFile('output.json', JSON.stringify(map, null, 4), function(err) {
+        res.send('Done');
+      });
+    }
+  });
+});
+
+app.listen('8081');
+
+console.log('UP');
+
+exports = module.exports = app;
